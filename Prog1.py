a092df0571b0d8e575de7b451deb805d02725b4f
Aryan9626 <155606773+Aryan9626@users.noreply.github.com>
2024-02-13 19:28:59 +0530

    Update Prog1.py

--- Prog1.py
+++ Prog1.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
-# Source code for summation of two numbers
+
+"""
+This code calculates the sum of all numbers in a given list.
+
+It raises a `ValueError` if the input list is empty, ensuring meaningful sum calculations.
+"""
 
 def summation(data):
-    return sum(data)
+    """
+    This function takes a list of numbers as input and returns their sum.
+
+    Args:
+        data (list): A list of numbers.
+
+    Returns:
+        int: The sum of the numbers in the list.
+
+    Raises:
+        ValueError: If the input list is empty.
+    """
+
+    if not data:
+        raise ValueError("The input list cannot be empty.")
+
+    total_sum = sum(data)
+
+    return total_sum
